db7ac4d04200eb14ae9d49376330e31f5c5489a3
Michael Silver <yomichael88@yahoo.com>
2015-05-18 15:04:00 -0400

    adds information to nodes and edges about rendering state and parent infobox

--- createNetwork.py
+++ createNetwork.py
@@ -1,6 +1,7 @@
 import json
 import networkx as nx
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# import pydot
 
 def main():
     with open('data/infoboxes.json', 'rb') as fp:
@@ -11,15 +12,42 @@ def main():
         for infobox, synonyms in infoboxAttributes.iteritems():
             for unrend, rend in synonyms.iteritems():
                 numberOfPairs += 1
-                G.add_edge(unrend, rend)
+
+                # NODES
+                rendState = 'unrend' # first deal with unrend
+                for node in (unrend, rend):
+                    # To convert into clean strings:
+                    # node = str(node.encode('ascii', 'ignore'))
+                    
+                    if node not in G: G.add_node(node) # if it's not there, add it
+
+                    # whether or not node existed before, it's there now
+                    if (infobox in G.node[node].keys()) and (rendState not in G.node[node][infobox]):
+                        # if infobox there, but rendState not there already, append rendState
+                        G.node[node][infobox].append(rendState)
+                    else:       # else (if infobox not there), begin a list with rendState
+                        # print 'called'
+                        G.node[node][infobox] = [rendState]
+                        if len(G.node[node][infobox]) == 2: print G.node[node][infobox]
+
+                    rendState = 'rend' # now we're dealing with rend
+
+
+                # EDGES
+                if not G.has_edge(unrend, rend): G.add_edge(unrend, rend) # if it's not there, add it
+                if ('infobox' in G.edge[unrend][rend].keys()) and (infobox not in G.edge[unrend][rend]['infobox']):
+                    G.edge[unrend][rend]['infobox'].append(infobox)
+                else:
+                    G.edge[unrend][rend]['infobox'] = [infobox]
+                
 
         print 'Saving graph...'
-        nx.write_gpickle(G, 'data/attributeSynonyms.gpickle')
+        nx.write_gpickle(G,'data/attributeSynonyms.gpickle')
         
         print 'DONE. Graphed a newtork of ' + str(G.number_of_nodes()) + ' nodes and ' + str(G.number_of_edges()) + ' edges from ' + str(numberOfPairs) + ' unrendered : rendered attribute pairs, spanning ' + str(len(infoboxAttributes.keys())) + ' infoboxes'
 
-        print 'Showing graph now.'
-        nx.draw_circular(G,with_labels=True)
-        plt.show()
+        # print 'Showing graph now.'
+        # nx.draw(G,with_labels=True)
+        # plt.show()
 
 main()
