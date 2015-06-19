4ff0cbb42da5df03775216afaa6dd8aad9c55576
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-06-19 21:13:45 +0300

    Turned it into a package.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='Attribute-Analyzer',
+    version='1.0',
+    description='Extract relationships from Wiki Infobox Attributes.',
+    author='Michael Silver',
+    author_email='msilver@csail.mit.edu',
+    url='https://github.com/michaelsilver/Attribute-Analyzer',
+    packages=['attribute_analyzer'],
+    install_requires=[
+        'xlrd',
+        'networkx',
+        'wikipediabase'
+    ],
+    dependency_links=[
+        # The upstream version on pypi will probably be out of date
+        # until this gets deployed
+        'git+https://github.com/fakedrake/wikipediabase.git#egg=wikipediabase'
+    ],
+    entry_points={
+        'console_scripts': [
+            'allinfoboxattributes = attribute_analyzer.allInfoboxAttributes:main',
+        ],
+    },
+
+)
