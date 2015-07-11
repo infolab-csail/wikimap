25128f76414d2fc28b1409af47f87a71e970461f
Michael Silver <yomichael88@yahoo.com>
2015-07-11 12:26:44 -0400

    first attempt at graph cleanup

--- attribute_analyzer/createNetwork.py
+++ attribute_analyzer/createNetwork.py
@@ -11,24 +11,72 @@ This program is part of "Attribute Analyzer," a system to extract
 relationships from Wiki infobox attributes
 """
 
-import sys
+import argparse
 import json
+from unidecode import unidecode
+import re
+import string
 import networkx as nx
 import synonym_network as sn
-# import matplotlib.pyplot as plt
-# import pydot
 
-def usage():
-    print __doc__
+badPunct = list(string.punctuation)
+badPunct.remove('_')            # will be dealt with separately (replaced with space)
+badPunct.remove('-')            # will be dealt with separately (replaced with space)
+badPunct.remove("'")            # will be dealt with separately (checking for 's)
+badPunct.remove('%')            # "% of total exports"
+badPunct.remove(',')            # "Managing editor, design"
+badPunct.remove('$')            # "MSRP US$"
+badPunct.remove('&')            # "Specific traits & abilities"
 
-def main(argv):
-    try:
-        mappingInput, graphOutput = argv[0], argv[1]
-    except IndexError:
-        usage()
-        sys.exit(2)
+html = re.compile("<.*?>")
+brackets = re.compile("\[.*?\]")
+parens = re.compile("\(.*?\)")
+noPossesive = re.compile("(?<!s)'(?!s)")
 
-    with open(mappingInput, 'rb') as fp:
+def clean(node):
+    node = unidecode(node)      # transliterate unicode
+
+    # leave the special '!!!!!' and 'File:' nodes alone, will be used as "bridges"
+    if '!!!!!' not in node and 'File:' not in node:
+        # strip all html, brackets, parens
+        node = re.sub(html, '', node)
+        node = re.sub(brackets, '', node)
+        node = re.sub(parens, '', node)
+        node = node.replace('&mdot;', '')
+
+        # check for possessive ('s or s' or s's); if none, strip all '
+        node = re.sub(noPossesive, '', node)
+
+        # replace all dashes and underscores with a space
+        node = node.replace('_', ' ')
+        node = node.replace('-', ' ')
+
+        # strip all remaining punctuation
+        for punct in badPunct:
+            node = node.replace(punct, '')
+
+        node = node.lower()           # lowercase
+        node = ' '.join(node.split()) # replace all multiple spaces with one
+    return node
+
+def addToField(location, field, value):
+    if (field in location.keys()) and (value not in location[field]):
+        location[field].append(value)
+    else:
+        location[field] = [value]
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--clean", help="make graph with clean nodes",
+                    action="store_true")
+    parser.add_argument("mappings", help="path to input JSON list of infoboxes")
+    parser.add_argument("graph", help="path to output network graph file")
+    # TODO:
+    #   - add function to clean nodes when -c is passed
+    #   - work on cleaning function
+    args = parser.parse_args()
+
+    with open(args.mappings, 'rb') as fp:
         infoboxAttributes = json.load(fp)
         G=sn.SynonymNetwork()
         numberOfPairs = 0
@@ -37,34 +85,27 @@ def main(argv):
             for unrend, rend in synonyms.iteritems():
                 numberOfPairs += 1
 
+                if args.clean:
+                    cleanVersion = {node:clean(node) for node in (unrend, rend)}
+                    for node in (unrend, rend):
+                        G.add_node(cleanVersion[node])
+                        addToField(G.node[cleanVersion[node]], 'was', node)
+                    unrend = cleanVersion[unrend]
+                    rend = cleanVersion[rend]
+
                 G.add_edge(unrend, rend)
-                # To convert into clean strings:
-                # node = str(node.encode('ascii', 'ignore'))
 
                 # NODE ATTRIBUTES
                 rendState = 'unrend' # first deal with unrend
                 for node in (unrend, rend):
-
-                    if (infobox in G.node[node].keys()) and (rendState not in G.node[node][infobox]):
-                        # if infobox there, but rendState not there already, append rendState
-                        G.node[node][infobox].append(rendState)
-                    else:       # else (if infobox not there), begin a list with rendState
-                        # print 'called'
-                        G.node[node][infobox] = [rendState]
-                        if len(G.node[node][infobox]) == 2: print G.node[node][infobox]
-
+                    addToField(G.node[node], infobox, rendState)
                     rendState = 'rend' # now we're dealing with rend
 
-
                 # EDGE ATTRIBUTES
-                if ('infobox' in G.edge[unrend][rend].keys()) and (infobox not in G.edge[unrend][rend]['infobox']):
-                    G.edge[unrend][rend]['infobox'].append(infobox)
-                else:
-                    G.edge[unrend][rend]['infobox'] = [infobox]
-                
+                addToField(G.edge[unrend][rend], 'infobox', infobox)
 
         print 'Saving graph...'
-        nx.write_gpickle(G, graphOutput)
+        nx.write_gpickle(G, args.graph)
         
         print 'DONE. Graphed a newtork of ' + str(G.number_of_nodes()) + ' nodes and ' + str(G.number_of_edges()) + ' edges from ' + str(numberOfPairs) + ' unrendered : rendered attribute pairs, spanning ' + str(len(infoboxAttributes.keys())) + ' infoboxes'
 
@@ -72,5 +113,5 @@ def main(argv):
         # nx.draw(G,with_labels=True)
         # plt.show()
 
-if __name__ == "__main__":
-    main(sys.argv[1:])
+if __name__ == '__main__':
+    main()
