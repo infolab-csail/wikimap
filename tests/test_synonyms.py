c9507a6f46833ac3104b11171f2e44c04b68b58e
Michael Silver <yomichael88@yahoo.com>
2015-08-08 08:48:27 -0400

    more specific errors, clean_wordnet public utility

--- tests/test_synonyms.py
+++ tests/test_synonyms.py
@@ -6,23 +6,40 @@ class TestWordNetUtils(unittest.TestCase):
 
     def test_clean_wordnet(self):
         self.assertEqual(
-            synonyms._clean_wordnet(u'Canis_familiaris'), 'canis familiaris')
+            synonyms.clean_wordnet(u'Canis_familiaris'), 'canis familiaris')
+
+
+class TestIdSynset(unittest.TestCase):
 
     def test_id_synset_single_key(self):
         self.assertItemsEqual(synonyms.id_synset('dog', ['canis familiaris']),
                               ['dog', 'domestic dog', 'canis familiaris'])
 
+    def test_id_synset_single_bad_key(self):
+        self.assertRaisesRegexp(
+            RuntimeError, "No synset of 'dog' matches keys",
+            synonyms.id_synset, 'dog', ['junk'])
+
+    def test_id_synset_bad_primary(self):
+        self.assertRaisesRegexp(
+            KeyError, "WordNet contains no synsets for word: asdf",
+            synonyms.id_synset, 'asdf', ['junk'])
+
     def test_id_synset_single_insufficient_key(self):
-        self.assertRaises(
-            RuntimeError, synonyms.id_synset, 'maintain', ['keep'])
+        self.assertRaisesRegexp(
+            RuntimeError,
+            "Keys insufficient to uniquely identify synset, \d synsets found",
+            synonyms.id_synset, 'maintain', ['keep'])
 
     def test_id_synset_many_keys(self):
         self.assertItemsEqual(synonyms.id_synset('maintain', ['keep', 'hold']),
                               ['keep', 'maintain', 'hold'])
 
     def test_id_synset_many_insufficient_keys(self):
-        self.assertRaises(
-            RuntimeError, synonyms.id_synset, 'maintain', ['keep', 'junk'])
+        self.assertRaisesRegexp(
+            RuntimeError,
+            "Keys insufficient to uniquely identify synset, \d synsets found",
+            synonyms.id_synset, 'maintain', ['keep', 'junk'])
 
     def tes_id_synset_many_oversufficient_keys(self):
         self.assertItemsEqual(
--- wikimap/synonyms.py
+++ wikimap/synonyms.py
@@ -2,7 +2,7 @@ from nltk.corpus import wordnet as wn
 from unidecode import unidecode
 
 
-def _clean_wordnet(word):
+def clean_wordnet(word):
     word = unidecode(word)
     word = word.replace('_', ' ')
     word = word.lower()
@@ -10,19 +10,24 @@ def _clean_wordnet(word):
 
 
 def id_synset(primary, keys):
-    candidates = [[_clean_wordnet(name) for name in synset.lemma_names()]
+    candidates = [[clean_wordnet(name) for name in synset.lemma_names()]
                   for synset in wn.synsets(primary)]
-    sublist = [x for x in candidates if keys[0] in x]
-    keys.pop(0)             # remove used key
-    while len(sublist) > 1 and len(keys) > 0:
-        # further narrow down
-        sub_sublist = [x for x in sublist if keys[0] in x]
-        if len(sub_sublist) > 0:
-            sublist = sub_sublist
-        keys.pop(0)             # remove used key
-
-    if len(sublist) > 1:
-        raise RuntimeError("Keys insufficient to uniquely identify synset, "
-                           + str(len(sublist)) + " synsets found")
+    if len(candidates) == 0:
+        raise KeyError("WordNet contains no synsets for word: " + primary)
     else:
-        return sublist[0]
+        sublist = [x for x in candidates if keys[0] in x]
+        keys.pop(0)             # remove used key
+        while len(sublist) > 1 and len(keys) > 0:
+            # further narrow down
+            sub_sublist = [x for x in sublist if keys[0] in x]
+            if len(sub_sublist) > 0:
+                sublist = sub_sublist
+            keys.pop(0)             # remove used key
+
+        if len(sublist) == 0:
+            raise RuntimeError("No synset of '" + primary + "' matches keys")
+        if len(sublist) > 1:
+            raise RuntimeError("Keys insufficient to uniquely identify synset, "
+                               + str(len(sublist)) + " synsets found")
+        else:
+            return sublist[0]
