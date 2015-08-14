b13462ff9ce0acca2e8d61aea45c45f81a919cec
Michael Silver <yomichael88@yahoo.com>
2015-08-14 11:44:10 -0700

    single word auto-lemmatization

--- wikimap/synonyms.py
+++ wikimap/synonyms.py
@@ -1,5 +1,9 @@
 from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer
+import nltk.tag
 from unidecode import unidecode
+
+lemmatizer = WordNetLemmatizer()
 
 
 def clean_wordnet(word):
@@ -31,3 +35,27 @@ def id_synset(primary, keys):
                                + str(len(sublist)) + " synsets found")
         else:
             return sublist[0]
+
+
+def get_wordnet_pos(treebank_tag):
+    if treebank_tag.startswith('J'):
+        return wn.ADJ
+    elif treebank_tag.startswith('V'):
+        return wn.VERB
+    elif treebank_tag.startswith('N'):
+        return wn.NOUN
+    elif treebank_tag.startswith('R'):
+        return wn.ADV
+    else:
+        raise KeyError("Treebank contains no such tag: " + treebank_tag)
+
+
+def auto_lemmatize(word):
+    # TODO: this handles single words nicely, but cannot do 'maintained by'
+    tagged = nltk.pos_tag([word])[0]
+    treebank_pos = tagged[1]
+    return lemmatizer.lemmatize(word, get_wordnet_pos(treebank_pos))
+
+
+# def get_synonyms(object_class, property, value_class):
+#     # stub
